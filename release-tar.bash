1824e5d4d41ca4484e5a2194f08adce05e700b32
Ken Seal <kas@ncgr.org>
2011-04-26 09:59:41 -0600

    Added release-tar.bash script for deploying species sites.

--- release-tar.bash
+++ release-tar.bash
@@ -1,7 +1,51 @@
 #!/bin/bash
+#
+# Create a tar ball of the species sites ready for deployment.
+# Ken Seal - NCGR
+#
 
-for dir in $(ls -d */)
+## Files to ignore ##
+SCRIPT=`basename $0`
+INFO="INFO"
+
+for dir in $(ls -d *)
 do
-    echo $dir
+    if [[ $dir == $SCRIPT || $dir == $INFO ]]
+    then
+        continue
+    else
+        # Omitted:
+        # log/
+        # startServer
+        # tmp
+        # data/ ## Retain uploaded files on the server.
+        tar czf $dir-release.tar.gz \
+            $dir/app \
+            $dir/config \
+            $dir/config.ru \
+            $dir/db \
+            $dir/doc \
+            $dir/Gemfile \
+            $dir/Gemfile.lock \
+            $dir/lib \
+            $dir/public \
+            $dir/Rakefile \
+            $dir/README \
+            $dir/script \
+            $dir/test \
+            $dir/vendor
+     fi
+done
+
+RELEASE="species-release.tar.gz"
+
+tar czf $RELEASE *.gz
+
+for tar in *.gz
+do
+    if [[ $tar != $RELEASE ]]
+    then
+        rm $tar
+    fi
 done
 

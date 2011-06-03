ef723bace3dc9856ebf076b91b9948e989966e6c
Ken Seal <kas@ncgr.org>
2011-06-03 16:53:37 -0600

    Single sign-out working properly. Undo experimental session store.

--- phavu/config/initializers/session_store.rb
+++ phavu/config/initializers/session_store.rb
@@ -1,10 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.env.production? ? domain = '.comparative-legumes.org' : domain = 'aztec.ncgr.org'
-
 Phavu::Application.config.session_store :active_record_store, 
   :key    => '_lis_site_session',
-  :domain => domain
+  :domain => :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information

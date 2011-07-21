1f7df672c5912919fff21bfdfdf02642624edbe5
Ken Seal <kas@ncgr.org>
2011-07-21 13:29:11 -0600

    Fixed admin edit reference_dataset bug. Added disable option to form helpers. Removed default value from is_public?. Refactored tests.

--- arahy/app/models/resource.rb
+++ arahy/app/models/resource.rb
@@ -7,4 +7,6 @@ class Resource < ActiveRecord::Base
   
   validates_format_of :url, :with => URI::regexp(%w(http https ftp)),
     :allow_blank => true, :message => ' is not valid. Please include a valid protocol.'
+  validates_presence_of :is_public, 
+    :message => ' is not set. Please select Yes or No.'
 end

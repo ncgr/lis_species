3cd7188efccde6bcb41465ef2feed318001c8258
Alex Rice <agr@ncgr.org>
2013-05-03 10:26:12 -0600

    updated release-tar script to list species direcories more gracefully and added some progress message.

--- release-tar.bash
+++ release-tar.bash
@@ -1,54 +1,39 @@
-#!/bin/bash
+#!/bin/bash -e
 #
 # Create a tar ball of the species sites for deployment.
-# Ken Seal - NCGR
+# Alex Rice, Ken Seal - NCGR
 #
 
-## Files to ignore ##
-SCRIPT=`basename $0`
-INFO="INFO"
-COPY="utils.bash"
-README="README.rdoc"
-
-for dir in $(ls -d *)
+# list only the subdirectories (these happen to be all species names for the site)
+for dir in $( ls -d */ | sed 's/\/$//' ) 
 do
-    if [[ $dir == $SCRIPT || $dir == $INFO || $dir == $COPY || $dir == $README ]]
-    then
-        continue
-    else
-        # Omitted:
-        # log
-        # startServer
-        # tmp
-        # .gitignore
-        # .rspec
-        tar czf $dir-release.tar.gz \
-            $dir/app \
-            $dir/config \
-            $dir/config.ru \
-            $dir/db \
-            $dir/doc \
-            $dir/Gemfile \
-            $dir/Gemfile.lock \
-            $dir/lib \
-            $dir/public \
-            $dir/Rakefile \
-            $dir/README.rdoc \
-            $dir/script \
-            $dir/spec \
-            $dir/vendor
-     fi
+  echo $dir
+  # Omitted files: log, startServer, tmp, .gitignore, .rspec
+  tar czf $dir-release.tar.gz \
+  $dir/app \
+  $dir/config \
+  $dir/config.ru \
+  $dir/db \
+  $dir/doc \
+  $dir/Gemfile \
+  $dir/Gemfile.lock \
+  $dir/lib \
+  $dir/public \
+  $dir/Rakefile \
+  $dir/README.rdoc \
+  $dir/script \
+  $dir/spec \
+  $dir/vendor
 done
 
 RELEASE="species-release.tar.gz"
-
 tar czf $RELEASE *.gz
-
+echo "*created $RELEASE*"
 for tar in *.gz
 do
-    if [[ $tar != $RELEASE ]]
+  if [[ $tar != $RELEASE ]]
     then
-        rm $tar
-    fi
+    echo "removing $tar"
+    rm $tar
+  fi
 done
-
